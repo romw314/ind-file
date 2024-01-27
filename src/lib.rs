3a229034a4d65adfedc06e292244cae6601f6369
romw314 <106016361+romw314@users.noreply.github.com>
2024-01-27 13:24:15 +0100

    Progress initialization moved to beginning

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,9 @@ use std::path::PathBuf;
 use indicatif::ProgressBar;
 
 pub fn write_with_progress(pb: &mut ProgressBar, bytes: &[u8], file_path: PathBuf, chunk_size: usize) -> anyhow::Result<()> {
-	let mut file = File::create(file_path)?;
 	pb.set_length(bytes.len().try_into()?);
 	pb.set_position(0);
+	let mut file = File::create(file_path)?;
 	for chunk in bytes.chunks(chunk_size) {
 		file.write_all(chunk)?;
 		pb.inc(chunk.len().try_into()?);
